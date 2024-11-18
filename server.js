c214999742e86a6989b42d3c489a57115bf53e23
camachoo18 <estivencamachops@gmail.com>
2024-11-18 09:57:58 +0100

    #build

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
+const port = 3000
+
+mongoose.connect("mongodb://localhost:27017/Juegos", {useNewUrlParser: true, useUnifiedTopology: true})
+
+const juegoSchema = new mongoose.Schema({
+    titulo: String,
+    desarrolladora: String,
+    "fecha-lanzamiento": String,
+    plataformas: Array
+})
+
+const Juego = mongoose.model('Juego', juegoSchema)
+
+app.get("/", async (req,res) => {
+    const juegos = await Juego.find()
+    res.send(juegos)
+})
+
+app.listen(port, () => {
+    console.log(`Servidor lanzado en ${port}`)
+})
